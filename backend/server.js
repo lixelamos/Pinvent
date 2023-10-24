e5ad558b783a772d5cdb6efd5dbdedea41be8a15
Amos <amosbett99@gmail.com>
2023-10-24 11:11:36 +0300

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
 const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const express= require("express");
-const mongoose= require("mongoose");
-const bodyParser= require("body-parser");
-const cors= require("cors");
+const cors = require("cors");
 
-const app =express()
+const app = express();
 
-const PORT =process.env .PORT ||5000
+// Use the correct port variable name, it should be `PORT` not `.PORT`
+const PORT = process.env.PORT || 5000;
 
-//connect DB and start server
+// Use a template string to display the port in the console log
 mongoose
-.connect(process.env.MONGO_URI)
-.then(()=>{
-    app.listen(PORT,() =>{
-        console.log('server running on port ${PORT}')
-    })
-}) 
-.catch((error))
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error)); // Log the error message
